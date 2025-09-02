5bd3d9599f7e93e8e2c4717e7128e9aec1d96174
viktor <ashenarx@gmail.com>
2025-09-03 01:05:44 +1000

    feat(hw01): implement CsvQuestionDao reading CSV

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,44 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        String fileName = fileNameProvider.getTestFileName();
 
-        return new ArrayList<>();
+        try (var inputStream = new ClassPathResource(fileName).getInputStream();
+             var reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+            List<QuestionDto> dtos = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withSkipLines(1)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .withIgnoreEmptyLine(true)
+                    .withThrowExceptions(true)
+                    .build()
+                    .parse();
+
+            return dtos.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (IOException | RuntimeException e) {
+            throw new QuestionReadException("Error reading questions from resource: " + fileName, e);
+        }
     }
 }
