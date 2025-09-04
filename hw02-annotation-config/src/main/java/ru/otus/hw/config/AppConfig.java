7a478f26b6276a1147fea869e642d5a93f515e34
viktor <ashenarx@gmail.com>
2025-09-05 00:02:01 +1000

    feat(hw02): add AppProperties bean configuration in AppConfig

--- hw02-annotation-config/src/main/java/ru/otus/hw/config/AppConfig.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/config/AppConfig.java
@@ -1,5 +1,7 @@
 package ru.otus.hw.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -7,4 +9,14 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @ComponentScan(basePackages = "ru.otus.hw")
 @PropertySource("classpath:application.properties")
-public class AppConfig { }
+public class AppConfig {
+
+    @Bean
+    public AppProperties appProperties(@Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
+                                       @Value("${test.fileName}") String testFileName) {
+        AppProperties properties = new AppProperties();
+        properties.setTestFileName(testFileName);
+        properties.setRightAnswersCountToPass(rightAnswersCountToPass);
+        return properties;
+    }
+}
