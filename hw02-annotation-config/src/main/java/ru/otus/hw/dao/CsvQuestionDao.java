b368c5895e48ff9206aa34e134dc3fa1695512b4
viktor <owlmajin@icloud.com>
2025-09-05 00:29:07 +1000

    refactor(hw02): extract helper methods in TestServiceImpl

--- hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -12,7 +12,6 @@ import ru.otus.hw.exceptions.QuestionReadException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -14,6 +14,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
+    private static final String HEADER = "Please answer the questions below%n";
+
+    private static final String INVALID_OPTION_MSG = "Invalid option. Try again";
+
+    private static final String OPTION_PROMPT_TEMPLATE = "Enter option number (1-%d):";
+
     private final IOService ioService;
 
     private final QuestionDao questionDao;
@@ -21,29 +27,46 @@ public class TestServiceImpl implements TestService {
     @Override
     public TestResult executeTestFor(Student student) {
         ioService.printLine("");
-        ioService.printFormattedLine("Please answer the questions below%n");
+        ioService.printFormattedLine(HEADER);
 
-        List<Question> questions = questionDao.findAll();
-        var testResult = new TestResult(student);
-
-        for (int i = 0; i < questions.size(); i++) {
-            var question = questions.get(i);
-            ioService.printFormattedLine("%d) %s", i + 1, question.text());
-
-            List<Answer> answers = question.answers();
-            for (int j = 0; j < answers.size(); j++) {
-                ioService.printFormattedLine("   %d) %s", j + 1, answers.get(j).text());
-            }
-
-            int choice = ioService.readIntForRangeWithPrompt(
-                    1,
-                    answers.size(),
-                    "Enter option number (1-" + answers.size() + "):",
-                    "Invalid option. Try again"
-            );
-            boolean isRight = answers.get(choice - 1).isCorrect();
-            testResult.applyAnswer(question, isRight);
+        final List<Question> questions = questionDao.findAll();
+        final var testResult = new TestResult(student);
+        if (questions.isEmpty()) {
+            return testResult;
         }
+
+        for (int q = 0; q < questions.size(); q++) {
+            final var question = questions.get(q);
+            printQuestion(question, q + 1);
+
+            final List<Answer> answers = question.answers();
+            printAnswers(answers);
+
+            final int choice = promptChoice(answers.size());
+            final boolean isCorrect = answers.get(choice - 1).isCorrect();
+
+            testResult.applyAnswer(question, isCorrect);
+        }
+
         return testResult;
+    }
+
+    private void printQuestion(Question question, int number) {
+        ioService.printFormattedLine("%d) %s", number, question.text());
+    }
+
+    private void printAnswers(List<Answer> answers) {
+        for (int i = 0; i < answers.size(); i++) {
+            ioService.printFormattedLine("   %d) %s", i + 1, answers.get(i).text());
+        }
+    }
+
+    private int promptChoice(int answersCount) {
+        return ioService.readIntForRangeWithPrompt(
+                1,
+                answersCount,
+                String.format(OPTION_PROMPT_TEMPLATE, answersCount),
+                INVALID_OPTION_MSG
+        );
     }
 }
