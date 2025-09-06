ee70edb9ab0b4c2c03d9ca20d3b58a32cb8a4010
viktor <ashenarx@gmail.com>
2025-09-07 00:22:46 +1000

    feat(hw03): implement TestServiceImpl with localization

--- hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,12 +3,22 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
+
+    private static final String QUESTION_KEY = "TestService.answer.the.questions";
+
+    private static final String ENTER_OPTION_KEY = "TestService.enter.option";
+
+    private static final String INVALID_OPTION_KEY = "TestService.invalid.option";
 
     private final LocalizedIOService ioService;
 
@@ -17,17 +27,40 @@ public class TestServiceImpl implements TestService {
     @Override
     public TestResult executeTestFor(Student student) {
         ioService.printLine("");
-        ioService.printLineLocalized("TestService.answer.the.questions");
+        ioService.printLineLocalized(QUESTION_KEY);
         ioService.printLine("");
 
-        var questions = questionDao.findAll();
-        var testResult = new TestResult(student);
-
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        final List<Question> questions = questionDao.findAll();
+        final var result = new TestResult(student);
+        if (questions.isEmpty()) {
+            return result;
         }
-        return testResult;
+
+        for (int i = 0; i < questions.size(); i++) {
+            final var question = questions.get(i);
+            printQuestion(question, i + 1);
+
+            final List<Answer> answers = question.answers();
+            printAnswers(answers);
+
+            int choice = ioService.readIntForRangeWithPromptLocalized(
+                    1, answers.size(), ENTER_OPTION_KEY, INVALID_OPTION_KEY
+            );
+
+            boolean isCorrect = answers.get(choice - 1).isCorrect();
+            result.applyAnswer(question, isCorrect);
+        }
+        return result;
+    }
+
+    private void printQuestion(Question question, int number) {
+        ioService.printFormattedLine("%d) %s", number, question.text());
+    }
+
+    private void printAnswers(List<Answer> answers) {
+        for (int i = 0; i < answers.size(); i++) {
+            ioService.printFormattedLine("   %d) %s", i + 1, answers.get(i).text());
+        }
     }
 
 }
