973c79a92ab5a5f62be42133fed45aa41e9c3585
viktor <ashenarx@gmail.com>
2025-09-10 03:10:59 +1000

    test(hw03): migrate CsvQuestionDaoTest from hw02 with content validation

--- hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
@@ -32,15 +33,13 @@ class CsvQuestionDaoTest {
     @DisplayName("should read questions from classpath resource and map to domain model")
     void shouldReadQuestionsFromResource() {
         // given
-        final var dao = new CsvQuestionDao(properties);
+        var dao = new CsvQuestionDao(properties);
 
         // when
         final List<Question> questions = dao.findAll();
 
         // then
-        assertThat(questions).isNotEmpty();
-        assertThat(questions.get(0).text()).isNotBlank();
-        assertThat(questions.get(0).answers()).isNotEmpty();
+        assertThat(questions).containsExactlyElementsOf(createSampleQuestions());
     }
 
     @Test
@@ -56,7 +55,7 @@ class CsvQuestionDaoTest {
     }
 
     @Test
-    @DisplayName("ru-RU locale maps to questions_ru.csv and parses Russian text")
+    @DisplayName("ru-RU locale maps to questions_ru.csv and parses Russian locale")
     void picksRussianCsv() {
         properties.setLocale("ru-RU");
 
@@ -68,6 +67,36 @@ class CsvQuestionDaoTest {
         assertThat(questions.get(0).text())
                 .isEqualTo("Есть ли жизнь на Марсе?");
         assertThat(questions.get(0).answers()).isNotEmpty();
+    }
+
+    private List<Question> createSampleQuestions() {
+        return List.of(
+                new Question(
+                        "Is there life on Mars?",
+                        List.of(
+                                new Answer("Science doesn't know this yet", true),
+                                new Answer("Certainly. The red UFO is from Mars. And green is from Venus", false),
+                                new Answer("Absolutely not", false)
+                        )
+                ),
+                new Question(
+                        "How should resources be loaded form jar in Java?",
+                        List.of(
+                                new Answer("ClassLoader#geResourceAsStream or ClassPathResource#getInputStream", true),
+                                new Answer("ClassLoader#geResource#getFile + FileReader", false),
+                                new Answer("Wingardium Leviosa", false)
+                        )
+                ),
+                new Question(
+                        "Which option is a good way to handle the exception?",
+                        List.of(
+                                new Answer("@SneakyThrow", false),
+                                new Answer("e.printStackTrace()", false),
+                                new Answer("Rethrow with wrapping in business exception (for example, QuestionReadException)", true),
+                                new Answer("Ignoring exception", false)
+                        )
+                )
+        );
     }
 
 }
