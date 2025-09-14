44c82cdb52fe42c64de1862e3c9e7512b4b5993a
viktor <ashenarx@gmail.com>
2025-09-15 00:11:16 +1000

    feat(hw06): add JPA entity mappings for Author, Genre, Book, and Comment

--- hw06-jpa/src/main/java/ru/otus/hw/models/Author.java
+++ hw06-jpa/src/main/java/ru/otus/hw/models/Author.java
@@ -1,14 +1,34 @@
 package ru.otus.hw.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(of = {"id", "fullName"})
 public class Author {
-    private long id;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "full_name")
     private String fullName;
+
 }
--- hw06-jpa/src/main/java/ru/otus/hw/models/Genre.java
+++ hw06-jpa/src/main/java/ru/otus/hw/models/Genre.java
@@ -1,14 +1,33 @@
 package ru.otus.hw.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
-@AllArgsConstructor
+@Entity
+@Table(name = "genres")
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(of = {"id", "name"})
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private long id;
 
+    @Column(name = "name")
     private String name;
 }
