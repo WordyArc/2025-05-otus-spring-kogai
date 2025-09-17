87e644fdece154ce6edc8c1c0293ed63ed402957
viktor <owlmajin@icloud.com>
2025-09-17 17:23:03 +1000

    style(hw06): fix checkstyle warnings

--- hw06-jpa/src/main/java/ru/otus/hw/models/Comment.java
+++ hw06-jpa/src/main/java/ru/otus/hw/models/Comment.java
@@ -44,15 +44,21 @@ public class Comment {
 
     @Override
     public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
         Class<?> oEffectiveClass = o instanceof HibernateProxy
                 ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
                 : o.getClass();
         Class<?> thisEffectiveClass = this instanceof HibernateProxy
                 ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
                 : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
         Comment comment = (Comment) o;
         return getId() != null && Objects.equals(getId(), comment.getId());
     }
