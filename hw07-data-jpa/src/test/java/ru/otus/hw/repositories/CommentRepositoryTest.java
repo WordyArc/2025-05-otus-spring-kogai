cbe9e6541de432f77cf54fe28a4bfb63aac6a90d
viktor <owlmajin@icloud.com>
2025-09-18 23:37:40 +1000

    test(hw07): rename repositories test classes after migration

--- hw07-data-jpa/src/test/java/ru/otus/hw/repositories/CommentRepositoryTest.java
+++ hw07-data-jpa/src/test/java/ru/otus/hw/repositories/CommentRepositoryTest.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-class JpaCommentRepositoryTest {
+class CommentRepositoryTest {
 
     @Autowired
     private CommentRepository commentRepository;
