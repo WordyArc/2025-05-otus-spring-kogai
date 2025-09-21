dbb85ebb5756de525c5da911384829108904b83e
viktor <owlmajin@icloud.com>
2025-09-21 17:35:13 +1000

    feat(hw06): add CRUD commands and converter for comments

---
+++ hw06-jpa/src/main/java/ru/otus/hw/converters/CommentConverter.java
@@ -0,0 +1,24 @@
+package ru.otus.hw.converters;
+
+import org.springframework.stereotype.Component;
+import ru.otus.hw.models.Comment;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+@Component
+public class CommentConverter {
+
+    public String commentToString(Comment c) {
+        var created = Optional.ofNullable(c.getCreatedAt())
+                .map(DateTimeFormatter.ISO_LOCAL_DATE_TIME::format)
+                .orElse("-");
+
+        var bookInfo = (c.getBook() == null)
+                ? "null"
+                : "id=%d, title=%s".formatted(c.getBook().getId(), c.getBook().getTitle());
+
+        return "Id: %d, book: [%s], text: %s, createdAt: %s"
+                .formatted(c.getId(), bookInfo, c.getText(), created);
+    }
+}
