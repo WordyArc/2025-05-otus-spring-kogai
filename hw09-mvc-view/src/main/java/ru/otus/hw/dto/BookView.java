8223871c1c8cfcd2e1433de4c237472126f2a5f4
viktor <ashenarx@gmail.com>
2025-09-23 10:58:15 +1000

    refactor(hw09): optimize `joinGenres` method in `BookView`

--- hw09-mvc-view/src/main/java/ru/otus/hw/dto/BookView.java
+++ hw09-mvc-view/src/main/java/ru/otus/hw/dto/BookView.java
@@ -1,12 +1,12 @@
 package ru.otus.hw.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record BookView(Long id, String title, AuthorDto author, List<GenreDto> genres) {
     public String joinGenres() {
         return genres == null ? "" : genres.stream()
                 .map(GenreDto::name)
-                .reduce((a, b) -> a + ", " + b)
-                .orElse("");
+                .collect(Collectors.joining(", "));
     }
 }
