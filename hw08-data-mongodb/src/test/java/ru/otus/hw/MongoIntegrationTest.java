0c7b37dfd60e635dbc691994caebec8b85e0955c
viktor <owlmajin@icloud.com>
2025-09-24 08:50:25 +1000

    feat(hw08): add configuration for MongoDB testing with embedded and container modes

--- hw08-data-mongodb/src/test/java/ru/otus/hw/MongoIntegrationTest.java
+++ hw08-data-mongodb/src/test/java/ru/otus/hw/MongoIntegrationTest.java
@@ -1,23 +1,13 @@
 package ru.otus.hw;
 
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import java.util.UUID;
 
-@Testcontainers(disabledWithoutDocker = true)
+@Import(MongoTestConfig.class)
 public abstract class MongoIntegrationTest {
-
-    private static final DockerImageName MONGO_IMAGE =
-            DockerImageName.parse("mongo:7.0.14");
-
-    @ServiceConnection
-    protected static final MongoDBContainer MONGO = new MongoDBContainer(MONGO_IMAGE)
-            .withReuse(true);
 
     @DynamicPropertySource
     static void overrideMongoProps(DynamicPropertyRegistry registry) {
---
+++ hw08-data-mongodb/src/test/java/ru/otus/hw/MongoTestConfig.java
@@ -0,0 +1,42 @@
+package ru.otus.hw;
+
+import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+
+@TestConfiguration(proxyBeanMethods = false)
+@Import({
+        MongoTestConfig.EmbeddedModeConfig.class,
+        MongoTestConfig.ContainerModeConfig.class
+})
+public class MongoTestConfig {
+
+    @TestConfiguration(proxyBeanMethods = false)
+    @ConditionalOnProperty(name = "test.mongo.mode", havingValue = "embedded", matchIfMissing = true)
+    static class EmbeddedModeConfig {
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    @ConditionalOnProperty(name = "test.mongo.mode", havingValue = "container")
+    @ImportAutoConfiguration(exclude = EmbeddedMongoAutoConfiguration.class)
+    static class ContainerModeConfig {
+
+        @Bean
+        @ServiceConnection
+        MongoDBContainer mongoDBContainer(
+                @Value("${test.mongo.image:mongo:7.0.14}") String image
+        ) {
+            return new MongoDBContainer(
+                    DockerImageName.parse(image)
+            );
+        }
+    }
+}
