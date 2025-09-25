f7dfae76403d0612737ff5e4aa027dbcef5825ef
viktor <owlmajin@icloud.com>
2025-09-25 23:35:32 +1000

    fix(hw10): remove redundant @RequiredArgsConstructor from view controllers

--- hw10-mvc-api/src/main/java/ru/otus/hw/controllers/view/GenreViewController.java
+++ hw10-mvc-api/src/main/java/ru/otus/hw/controllers/view/GenreViewController.java
@@ -1,11 +1,9 @@
 package ru.otus.hw.controllers.view;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class GenreViewController {
 
     @GetMapping("/genres")
