ebd255429a2cbab1b84e91040f1964b575cad18a
viktor <ashenarx@gmail.com>
2025-09-25 23:35:32 +1000

    fix(hw10): remove redundant @RequiredArgsConstructor from view controllers

--- hw10-mvc-api/src/main/java/ru/otus/hw/controllers/view/BookViewController.java
+++ hw10-mvc-api/src/main/java/ru/otus/hw/controllers/view/BookViewController.java
@@ -1,13 +1,11 @@
 package ru.otus.hw.controllers.view;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookViewController {
 
