37018a3073466b541d0810a7529a82cc7019d351
viktor <owlmajin@icloud.com>
2025-09-25 23:35:16 +1000

    fix(hw10): simplify ViewExceptionHandler by removing redundant exception handlers

--- hw10-mvc-api/src/main/java/ru/otus/hw/exceptions/ViewExceptionHandler.java
+++ hw10-mvc-api/src/main/java/ru/otus/hw/exceptions/ViewExceptionHandler.java
@@ -1,40 +1,21 @@
 package ru.otus.hw.exceptions;
 
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice(basePackages = "ru.otus.hw.controllers.view")
 public class ViewExceptionHandler {
 
-    @ExceptionHandler({EntityNotFoundException.class, NoSuchElementException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFound(Model model) {
-        model.addAttribute("status", 404);
-        model.addAttribute("message", "Not Found");
-        return "error";
-    }
-
-    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class, ConstraintViolationException.class})
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidation(Model model) {
+    String handleTypeMismatch(Model model) {
         model.addAttribute("status", 400);
-        model.addAttribute("message", "Validation error");
+        model.addAttribute("message", "Invalid parameter");
         return "error";
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleGeneric(Model model) {
-        model.addAttribute("status", 500);
-        model.addAttribute("message", "Unexpected error");
-        return "error";
-    }
 }
