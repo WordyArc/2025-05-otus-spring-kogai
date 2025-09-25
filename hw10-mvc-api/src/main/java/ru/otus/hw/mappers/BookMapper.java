1ef98c95eb2397a095d7900876fd300284d6066c
viktor <owlmajin@icloud.com>
2025-09-25 16:43:54 +1000

    refactor(hw10): consolidate mapping logic into `BookDto`

--- hw10-mvc-api/src/main/java/ru/otus/hw/mappers/BookMapper.java
+++ hw10-mvc-api/src/main/java/ru/otus/hw/mappers/BookMapper.java
@@ -2,31 +2,19 @@ package ru.otus.hw.mappers;
 
 import lombok.experimental.UtilityClass;
 import ru.otus.hw.dto.AuthorDto;
-import ru.otus.hw.dto.BookFormDto;
-import ru.otus.hw.dto.BookView;
+import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.GenreDto;
 import ru.otus.hw.models.Book;
-import ru.otus.hw.models.Genre;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @UtilityClass
 public class BookMapper {
 
-    public BookView toView(Book b) {
+    public BookDto toDto(Book b) {
         var authorDto = new AuthorDto(b.getAuthor().getId(), b.getAuthor().getFullName());
         var genres = b.getGenres().stream()
                 .map(g -> new GenreDto(g.getId(), g.getName()))
                 .toList();
-        return new BookView(b.getId(), b.getTitle(), authorDto, genres);
-    }
-
-    public BookFormDto toFormDto(Book b) {
-        Set<Long> gIds = b.getGenres().stream()
-                .map(Genre::getId)
-                .collect(Collectors.toSet());
-        return new BookFormDto(b.getId(), b.getTitle(), b.getAuthor().getId(), gIds);
+        return new BookDto(b.getId(), b.getTitle(), authorDto, genres);
     }
 
 }
