6011fe7efad0442a118823d76882bff4def71f78
viktor <owlmajin@icloud.com>
2025-09-27 05:02:31 +1000

    refactor(hw08): restructure test data and context configuration

--- hw08-data-mongodb/src/test/java/ru/otus/hw/CommonContext.java
+++ hw08-data-mongodb/src/test/java/ru/otus/hw/CommonContext.java
@@ -3,15 +3,17 @@ package ru.otus.hw;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import ru.otus.hw.config.MongoTestConfig;
 
 import java.util.UUID;
 
+
 @Import(MongoTestConfig.class)
-public abstract class MongoIntegrationTest {
+public abstract class CommonContext {
 
     @DynamicPropertySource
     static void overrideMongoProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.database", MongoIntegrationTest::randomDbName);
+        registry.add("spring.data.mongodb.database", CommonContext::randomDbName);
     }
 
     private static String randomDbName() {
