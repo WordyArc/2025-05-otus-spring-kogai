765ce237305558a617141b00a399290133738ccd
viktor <ashenarx@gmail.com>
2025-09-29 13:36:31 +1000

    feat(hw11): implement custom repository for Book aggregates with R2DBC

---
+++ hw11-webflux/src/main/java/ru/otus/hw/repositories/BookRepositoryCustom.java
@@ -0,0 +1,14 @@
+package ru.otus.hw.repositories;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.otus.hw.models.Book;
+
+public interface BookRepositoryCustom {
+
+    Flux<Book> findAllAggregates();
+
+    Mono<Book> findAggregateById(Long id);
+
+    Mono<Void> replaceGenres(Long bookId, Iterable<Long> genreIds);
+}
