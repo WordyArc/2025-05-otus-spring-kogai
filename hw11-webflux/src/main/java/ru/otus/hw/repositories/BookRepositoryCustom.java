95b2526a6ca150086cabbc12808f18301e505c15
viktor <owlmajin@icloud.com>
2025-09-30 23:49:49 +1000

    refactor(hw11): consolidate custom repository implementations

--- hw11-webflux/src/main/java/ru/otus/hw/repositories/BookRepositoryCustom.java
+++ hw11-webflux/src/main/java/ru/otus/hw/repositories/BookRepositoryCustom.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.otus.hw.models.Book;
 
-public interface BookAggregateRepository {
+public interface BookRepositoryCustom {
 
     Flux<Book> findAllAggregates();
 
