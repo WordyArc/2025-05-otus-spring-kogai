21cb786582f58870d8d0407f4c43600ed44162e2
viktor <owlmajin@icloud.com>
2025-10-03 05:54:24 +1000

    revert(hw12): partial commit `78a71c07`

--- hw09-mvc-view/src/main/java/ru/otus/hw/mappers/CommentMapper.java
+++ hw09-mvc-view/src/main/java/ru/otus/hw/mappers/CommentMapper.java
@@ -2,14 +2,13 @@ package ru.otus.hw.mappers;
 
 import lombok.experimental.UtilityClass;
 import ru.otus.hw.dto.CommentDto;
-import ru.otus.hw.models.Comment;
 
 import java.time.format.DateTimeFormatter;
 
 @UtilityClass
 public class CommentMapper {
 
-    public CommentDto toDto(Comment c) {
+    public CommentDto toDto(ru.otus.hw.models.Comment c) {
         var created = c.getCreatedAt() == null
                 ? "-"
                 : DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(c.getCreatedAt());
