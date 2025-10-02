6ed335c9074dbe244f12d94e6e47c184469de919
viktor <owlmajin@icloud.com>
2025-10-03 03:55:16 +1000

    test(hw12): disable security filters in controller tests

--- hw12-security-form/src/test/java/ru/otus/hw/controllers/view/BookViewControllerTest.java
+++ hw12-security-form/src/test/java/ru/otus/hw/controllers/view/BookViewControllerTest.java
@@ -3,6 +3,7 @@ package ru.otus.hw.controllers.view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(controllers = BookViewController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class BookViewControllerTest {
 
     @Autowired
