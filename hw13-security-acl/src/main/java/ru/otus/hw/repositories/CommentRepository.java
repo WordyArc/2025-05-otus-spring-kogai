6654c7401b7b39c82b7c4d5c13d0ce6cb5a4576e
viktor <ashenarx@gmail.com>
2025-10-03 18:15:58 +1000

    feat(hw13): initialize hw13-security-acl project template

---
+++ hw13-security-acl/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -0,0 +1,18 @@
+package ru.otus.hw.repositories;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = "book")
+    Optional<Comment> findById(Long id);
+
+    @EntityGraph(attributePaths = "book")
+    List<Comment> findAllByBookId(Long bookId);
+}
