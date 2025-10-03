f953232c26325e579d838e3586719ad1ec26ba5a
viktor <owlmajin@icloud.com>
2025-10-03 18:15:58 +1000

    feat(hw13): initialize hw13-security-acl project template

---
+++ hw13-security-acl/src/main/java/ru/otus/hw/services/CommentService.java
@@ -0,0 +1,18 @@
+package ru.otus.hw.services;
+
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentService {
+    Optional<Comment> findById(Long id);
+
+    List<Comment> findAllByBookId(Long bookId);
+
+    Comment create(Long bookId, String text);
+
+    Comment update(Long id, String newText);
+
+    void deleteById(Long id);
+}
