abf7e10d43f517e11dbf135ad3c973711011cd5f
viktor <owlmajin@icloud.com>
2025-10-03 23:53:09 +1000

    refactor(hw13): simplify Role enum and adjust SecurityConfig for authentication-based access control

--- hw13-security-acl/src/main/java/ru/otus/hw/models/Role.java
+++ hw13-security-acl/src/main/java/ru/otus/hw/models/Role.java
@@ -1,12 +1,6 @@
 package ru.otus.hw.models;
 
 public enum Role {
-    ROLE_USER("ROLE_USER"),
-    ROLE_ADMIN("ROLE_ADMIN");
-
-    public final String name;
-
-    Role(String name) {
-        this.name = name;
-    }
+    ROLE_USER,
+    ROLE_ADMIN;
 }
