1007a6d912434a0f5dadb86a527ce23a5c569726
viktor <owlmajin@icloud.com>
2025-10-03 22:28:32 +1000

    feat(hw13): add AclBookService for managing ACLs of Book entities

--- hw13-security-acl/src/main/java/ru/otus/hw/models/Role.java
+++ hw13-security-acl/src/main/java/ru/otus/hw/models/Role.java
@@ -1,6 +1,12 @@
 package ru.otus.hw.models;
 
 public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    public final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
 }
