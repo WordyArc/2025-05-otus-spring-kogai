b47abad8e85042ec8e76bde93294e53739b90cc9
viktor <owlmajin@icloud.com>
2025-10-04 13:29:50 +1000

    style(hw10): resolve checkstyle errors

--- hw10-mvc-api/src/main/java/ru/otus/hw/controllers/api/CommentController.java
+++ hw10-mvc-api/src/main/java/ru/otus/hw/controllers/api/CommentController.java
@@ -2,7 +2,11 @@ package ru.otus.hw.controllers.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import ru.otus.hw.dto.CommentDto;
 import ru.otus.hw.mappers.CommentMapper;
