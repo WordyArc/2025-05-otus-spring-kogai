e54aab40620af6e3a7e7f46792048e050fc8c098
viktor <owlmajin@icloud.com>
2025-10-07 00:31:25 +1000

    refactor(hw14): update repositories and data seeding to use ObjectId

--- hw14-spring-batch/src/main/java/ru/otus/hw/mongo/repositories/MongoCommentRepository.java
+++ hw14-spring-batch/src/main/java/ru/otus/hw/mongo/repositories/MongoCommentRepository.java
@@ -1,12 +1,13 @@
 package ru.otus.hw.mongo.repositories;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ru.otus.hw.mongo.models.CommentDocument;
 
 import java.util.List;
 
-public interface MongoCommentRepository extends MongoRepository<CommentDocument, String> {
-    List<CommentDocument> findAllByBookId(String bookId);
+public interface MongoCommentRepository extends MongoRepository<CommentDocument, ObjectId> {
+    List<CommentDocument> findAllByBookId(ObjectId bookId);
     
-    void deleteByBookId(String bookId);
+    void deleteByBookId(ObjectId bookId);
 }
