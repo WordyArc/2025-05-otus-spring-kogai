c5573ece9fcfd3f3f5a67a77c12dc2f22d03f779
viktor <ashenarx@gmail.com>
2025-10-07 01:54:49 +1000

    feat(hw14): increase cache size and simplify ID resolution logic in IdMappingService

--- hw14-spring-batch/src/main/java/ru/otus/hw/migration/service/IdMappingService.java
+++ hw14-spring-batch/src/main/java/ru/otus/hw/migration/service/IdMappingService.java
@@ -25,7 +25,7 @@ public class IdMappingService {
     private final MongoOperations operations;
 
     private final Cache<@NonNull String, ObjectId> cache = Caffeine.newBuilder()
-            .maximumSize(200_000)
+            .maximumSize(500_000)
             .build();
 
     public ObjectId resolve(String srcType, String srcId) {
@@ -33,23 +33,18 @@ public class IdMappingService {
         ObjectId cached = cache.getIfPresent(key);
         if (cached != null) return cached;
 
-        var existing = repository.findBySourceTypeAndSourceId(srcType, srcId).map(IdMappingDocument::getTargetId);
-        if (existing.isPresent()) {
-            cache.put(key, existing.get());
-            return existing.get();
-        }
-
+        var query = Query.query(Criteria.where("sourceType").is(srcType).and("sourceId").is(srcId));
         var newId = new ObjectId();
-        var query = new Query(Criteria.where("sourceType").is(srcType).and("sourceId").is(srcId));
         var update = new Update()
                 .setOnInsert("sourceType", srcType)
                 .setOnInsert("sourceId", srcId)
                 .setOnInsert("targetId", newId);
-        var opts = FindAndModifyOptions.options().returnNew(true).upsert(true);
 
-        var saved = operations.findAndModify(query, update, opts, IdMappingDocument.class, "id_mappings");
-        ObjectId resolved = Objects.requireNonNull(saved).getTargetId();
-        cache.put(key, resolved);
-        return resolved;
+        var opts = FindAndModifyOptions.options().upsert(true).returnNew(true);
+        var saved = Objects.requireNonNull(
+                operations.findAndModify(query, update, opts, IdMappingDocument.class, "id_mappings")
+        );
+
+        return saved.getTargetId();
     }
 }
