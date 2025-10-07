558fc808bf41411265a21cbefc8ed872aa1513fc
viktor <ashenarx@gmail.com>
2025-10-07 19:16:46 +1000

    style(hw14): fix checkstyle errors

--- hw14-spring-batch/src/main/java/ru/otus/hw/config/BatchProperties.java
+++ hw14-spring-batch/src/main/java/ru/otus/hw/config/BatchProperties.java
@@ -23,30 +23,40 @@ public class BatchProperties {
     @Data
     public static class IdMappingCache {
         private long maximumSize = 500_000;
+
         private Map<String, Long> perType = new HashMap<>();
     }
 
     @Data
     public static class ThreadPool {
         private String threadNamePrefix = "batch-";
+
         private int corePoolSize = 4;
+
         private int maxPoolSize = 8;
+
         private int queueCapacity = 0;
     }
 
     @Data
     public static class ChunkSizes {
         private int authors = 500;
+
         private int genres = 1000;
+
         private int books = 200;
+
         private int comments = 5000;
     }
 
     @Data
     public static class ProgressListeners {
         private int authors = 5_000;
+
         private int genres = 200;
+
         private int books = 10_000;
+
         private int comments = 20_000;
     }
 }
