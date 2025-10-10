3368dcf6a5c0f7f892d587179172e951631e643b
viktor <ashenarx@gmail.com>
2025-10-11 03:35:34 +1000

    refactor(hw16): update repository annotations for usage HATEOAS projection

--- hw16-actuator-hateoas/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
+++ hw16-actuator-hateoas/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Author;
 
-@RepositoryRestResource(path = "authors", collectionResourceRel = "authors", itemResourceRel = "author")
-public interface AuthorRepository extends JpaRepository<Author, Long> { }
+@RepositoryRestResource(
+        path = "authors",
+        collectionResourceRel = "authors",
+        itemResourceRel = "author"
+)
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
