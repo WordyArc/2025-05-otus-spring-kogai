c831bbc0994cadda76a95ab598b0e5785d596416
viktor <owlmajin@icloud.com>
2025-10-11 03:35:34 +1000

    refactor(hw16): update repository annotations for usage HATEOAS projection

--- hw16-actuator-hateoas/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw16-actuator-hateoas/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -4,11 +4,17 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Book;
+import ru.otus.hw.projections.BookDetailProjection;
 
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource(path = "books", collectionResourceRel = "books", itemResourceRel = "book")
+@RepositoryRestResource(
+        path = "books",
+        collectionResourceRel = "books",
+        itemResourceRel = "book",
+        excerptProjection = BookDetailProjection.class
+)
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Override
