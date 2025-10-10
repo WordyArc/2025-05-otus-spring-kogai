85f84efce3584cb23c0b1fca12429a54ae0264b2
viktor <owlmajin@icloud.com>
2025-10-10 20:00:16 +1000

    feat(hw16): initialize hw16-actuator-hateoas project

---
+++ hw16-actuator-hateoas/src/main/java/ru/otus/hw/controllers/GenreController.java
@@ -0,0 +1,26 @@
+package ru.otus.hw.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.otus.hw.dto.GenreDto;
+import ru.otus.hw.services.GenreService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/genres")
+public class GenreController {
+
+    private final GenreService genreService;
+
+    @GetMapping
+    public List<GenreDto> genres() {
+        return genreService.findAll().stream()
+                .map(g -> new GenreDto(g.getId(), g.getName()))
+                .toList();
+    }
+}
